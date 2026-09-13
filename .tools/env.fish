fd7390dab90ef517c3fd2177846d4f64169f3447
Miralthor <nik@truninger.net>
2026-09-13 14:21:29 +0200

    chore: remove local artifacts and add ignore rules

--- .tools/env.fish
+++
@@ -1,4 +0,0 @@
-if not contains "$HOME/dev/songmirror/.tools" $PATH
-    # Prepending path in case a system-installed binary needs to be overridden
-    set -x PATH "$HOME/dev/songmirror/.tools" $PATH
-end
